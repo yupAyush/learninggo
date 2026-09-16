11bffe8f6115feae745944fed34d7834712579d0
yupAyush <kratikmishra2005@gmail.com>
2026-09-16 23:36:51 +0530

    Rename main function in typeassertion.go and add new typeswitches.go with type switch example

--- interface/typeassertion.go
+++ interface/typeassertion.go
@@ -4,7 +4,7 @@ import "fmt"
 
 //type assertion is used to retrieve the dynamic value of an interface. It allows you to access the underlying concrete value stored in an interface variable. The syntax for type assertion is as follows:
 
-func main() {
+func main7() {
 	var i interface{} = "hello"
 	s := i.(string)
 	fmt.Println(s)
