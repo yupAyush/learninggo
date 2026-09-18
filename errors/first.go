6a0ac1a1e5c6e91ee08552a7ce9379d7d2d2117c
yupAyush <kratikmishra2005@gmail.com>
2026-09-18 23:45:38 +0530

    Add detailed comments explaining custom error implementation in first.go

--- errors/first.go
+++ errors/first.go
@@ -23,3 +23,5 @@ func main() {
 		fmt.Println(err)
 	}
 }
+
+//errors in golang is are implemented using the error interface. The error interface has a single method, Error(), which returns a string. In this example, we define a custom error type MyError that implements the error interface by providing an Error() method. The run() function returns an instance of MyError, and in the main function, we check for errors and print them if they occur.
